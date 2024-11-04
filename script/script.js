327e386819faf002ab75cc31c37b91e4a0acd6e2
Arché AKABI NANGA <arc.aka@dwwm.evx>
2024-11-04 16:44:02 +0000

    Init_Js

---
+++ script/script.js
@@ -0,0 +1,84 @@
+// var compteur=2;
+// function maFonction()
+// {
+//     var myVar=456;
+//     console.log("myVar: "+myVar);
+// }
+// maFonction()
+
+// if (compteur > 1)
+// {
+//     let myVar2="Wazza !";
+//     console.log("myVar2 : "+myVar2);
+// }
+
+
+// console.log(typeof myVar);
+
+// document.write("Votre âge est ");
+// let age=35;
+// let myLet=age;
+// document.write(age);
+// console.log("myLet:"+myLet);
+
+
+
+// MyVar = "Bonjour"; 
+// var myVar="Bonjour";
+// window.alert(myVar); 
+// window.alert("chaîne de caractères"); 
+// window.alert(myVar + "chaîne de caractères");
+// console.log("myVar:" +myVar);
+
+
+// var reponse1 = window.prompt("Saisissez votre nom"); 
+// var reponse2 = window.prompt("Saisissez votre couleur préférée");
+
+// if (window.confirm("Voulez-vous continuer ?") == true)
+// {
+
+// }
+
+
+// alert("Vous avez confirmé !");
+// console.log("alert : vous avez confirmé")
+
+
+// var temperature=37
+// if(window.prompt("temperature") > 37)
+// {
+//     alert("hospitaliser le patient");
+// }
+
+// var reponse = "oui";
+// if (reponse == "oui")
+
+// {
+// console.log("Bonne réponse !");
+// }
+
+
+// // Condition avec ET
+// let region="Ile de France"
+// let ville="Paris"
+// let Pays="France"
+
+// if (region == "Ile de France" && ville == "Paris")
+// {
+// console.log("Pays : France");
+
+// // Condition avec OU
+// if (region == "Ile de France" || pays == "France")
+// {
+// console.log("Pays : France");
+// }
+
+let reponse="oui"
+if (reponse == "oui")
+    {
+    console.log("Bonne réponse !");
+    }
+    else
+    {
+    console.log("Mauvaise réponse !");
+    }
